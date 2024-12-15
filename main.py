2d328497f66b2e9b0c360fe21f30ae841d04dda5
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-12-16 00:56:56 +0200

    add env.template

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# Import user routes
 from crud.user import router as user_router
 from crud.appointment import router as appointment_router
 from crud.services import router as service_router
@@ -18,16 +17,15 @@ app = FastAPI(
     redoc_url="/redoc",
 )
 
-# CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Дозволяє запити з будь-яких джерел
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Дозволяє всі HTTP-методи
-    allow_headers=["*"],  # Дозволяє всі заголовки
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Include routers
+
 app.include_router(user_router, prefix="/api/v1")
 app.include_router(appointment_router, prefix="/api/v1")
 app.include_router(service_router, prefix="/api/v1")
